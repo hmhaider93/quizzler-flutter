b16a064dd2673afda994ba3e395beb7096817b52
Haider Mushtaq <hmhaider93@gmail.com>
2025-03-25 22:33:14 +0100

    Creating the Question Class and adding data to it.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quizzler/questions.dart';
 
 void main() => runApp(Quizzler());
 
@@ -32,6 +33,12 @@ class _QuizPageState extends State<QuizPage> {
     'Approximately one quarter of human bones are in the feet.',
     'A slug\'s blood is green.',
   ];
+  List<Question> questionData = [
+    Question(questionText: 'You can lead a cow down stairs but not up stairs.', questionAnswer: false), 
+    Question(questionText: 'Approximately one quarter of human bones are in the feet.', questionAnswer: true), 
+    Question(questionText: 'A slug\'s blood is green.', questionAnswer: true), 
+    Question(questionText: 'A slug\'s blood is Orange.', questionAnswer: false), 
+  ];
   List<Icon> scoreKeeper = [];
 
   @override
@@ -46,7 +53,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questionList[questionNumber],
+                questionData[questionNumber].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -72,8 +79,13 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 setState(() {
+
+                if (questionData[questionNumber].questionAnswer == true) {
+                  scoreKeeper.add(Icon(Icons.check, color: Colors.green, size: 32,));
+                } else {
+                  scoreKeeper.add(Icon(Icons.close, color: Colors.red, size: 32,));
+                }
                 questionNumber++;
-                scoreKeeper.add(Icon(Icons.check, color: Colors.green, size: 32,));
                 });
               },
             ),
@@ -95,7 +107,13 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 setState(() {
-                scoreKeeper.add(Icon(Icons.close, color: Colors.red, size: 32,));
+
+                if (questionData[questionNumber].questionAnswer == false) {
+                  scoreKeeper.add(Icon(Icons.check, color: Colors.green, size: 32,));
+                } else {
+                  scoreKeeper.add(Icon(Icons.close, color: Colors.red, size: 32,));
+                }
+                questionNumber++;
                 });
               },
             ),
