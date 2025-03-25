c9ab6f79112463911c615c99f49fea8a7d925154
Haider Mushtaq <hmhaider93@gmail.com>
2025-03-25 21:53:22 +0100

    Adding Question Lists.

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,15 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+
+  int questionNumber = 0;
+  List<String> questionList = [
+    'You can lead a cow down stairs but not up stairs.', 
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.',
+  ];
+  List<Icon> scoreKeeper = [];
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +46,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questionList[questionNumber],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -62,7 +71,10 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked true.
+                setState(() {
+                questionNumber++;
+                scoreKeeper.add(Icon(Icons.check, color: Colors.green, size: 32,));
+                });
               },
             ),
           ),
@@ -82,12 +94,16 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked false.
+                setState(() {
+                scoreKeeper.add(Icon(Icons.close, color: Colors.red, size: 32,));
+                });
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children: scoreKeeper,
+        )
       ],
     );
   }
